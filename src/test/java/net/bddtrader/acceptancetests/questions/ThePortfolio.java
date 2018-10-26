b8266603cce4a028fa428e9cf4e3fd5c21c3f610
DHIII <dh3code@gmail.com>
2018-10-26 08:17:12 -0400

    Completed excersise 3

--- src/test/java/net/bddtrader/acceptancetests/questions/ThePortfolio.java
+++ src/test/java/net/bddtrader/acceptancetests/questions/ThePortfolio.java
@@ -10,31 +10,39 @@ import java.util.List;
 
 public class ThePortfolio {
 
-    public static Question<Float> cashBalanceFor(Client client) {
+  public static Question<Float> cashBalanceFor(Client client) {
 
-        return new RestQuestionBuilder<Float>().about("Cash account balance")
-                .to(BDDTraderEndPoints.ClientPortfolio.path())
-                .with(request -> request.pathParam("clientId", client.getId()))
-                .returning(response -> response.path("cash"));
+    return new RestQuestionBuilder<Float>().about("Cash account balance")
+            .to(BDDTraderEndPoints.ClientPortfolio.path())
+            .with(request -> request.pathParam("clientId", client.getId()))
+            .returning(response -> response.path("cash"));
 
-    }
+  }
 
-    public static Question<Integer> idFor(Client client) {
+  public static Question<Integer> idFor(Client client) {
 
-        return new RestQuestionBuilder<Integer>().about("Cash account balance")
-                .to(BDDTraderEndPoints.ClientPortfolio.path())
-                .with(request -> request.pathParam("clientId", client.getId()))
-                .returning(response -> response.path("portfolioId"));
+    return new RestQuestionBuilder<Integer>().about("Cash account balance")
+            .to(BDDTraderEndPoints.ClientPortfolio.path())
+            .with(request -> request.pathParam("clientId", client.getId()))
+            .returning(response -> response.path("portfolioId"));
 
-    }
+  }
 
-    public static Question<List<Position>> positionsForClient(Client registeredClient) {
+  public static Question<List<Position>> positionsForClient(Client registeredClient) {
 
-        return new RestQuestionBuilder<List<Position>>()
-                .about("positions for client " + registeredClient)
-                .to(BDDTraderEndPoints.ClientPortfolioPositions.path())
-                .with(request -> request.pathParam("clientId", registeredClient.getId()))
-                .returning(response -> response.jsonPath().getList("", Position.class));
+    return new RestQuestionBuilder<List<Position>>()
+            .about("positions for client " + registeredClient)
+            .to(BDDTraderEndPoints.ClientPortfolioPositions.path())
+            .with(request -> request.pathParam("clientId", registeredClient.getId()))
+            .returning(response -> response.jsonPath().getList("", Position.class));
 
-    }
+  }
+
+  public static Question<Double> overallProfitForPortfolioId(Integer portfolioId) {
+
+    return new RestQuestionBuilder<Double>().about("Overall profit")
+            .to(BDDTraderEndPoints.PortfolioProfit.path())
+            .withPathParameters("portfolioId", portfolioId)
+            .returning(response -> response.as(Double.class));
+  }
 }
--- src/test/java/net/bddtrader/acceptancetests/stepdefinitions/ViewingPositionsStepDefinitons.java
+++ src/test/java/net/bddtrader/acceptancetests/stepdefinitions/ViewingPositionsStepDefinitons.java
@@ -1,17 +1,25 @@
 package net.bddtrader.acceptancetests.stepdefinitions;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import net.bddtrader.acceptancetests.endpoints.BDDTraderEndPoints;
 import net.bddtrader.acceptancetests.model.MarketPrice;
+import net.bddtrader.acceptancetests.questions.ThePortfolio;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.rest.interactions.Get;
 import net.serenitybdd.screenplay.rest.interactions.Post;
 
 import java.util.List;
 import java.util.Map;
 
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class ViewingPositionsStepDefinitons {
 
@@ -25,10 +33,25 @@ public class ViewingPositionsStepDefinitons {
     private void updateMarketPrice(MarketPrice marketPrice) {
         theActorCalled("Market Forces").attemptsTo(
                 Post.to(BDDTraderEndPoints.UpdatePrice.path())
-                     .with(request -> request.pathParam("securityCode", marketPrice.getSecurityCode())
-                                             .header("Content-Type", "application/json")
-                                             .body(marketPrice.getPrice()))
+                        .with(request -> request.pathParam("securityCode", marketPrice.getSecurityCode())
+                                .header("Content-Type", "application/json")
+                                .body(marketPrice.getPrice()))
         );
         assertThat(SerenityRest.lastResponse().statusCode()).isEqualTo(200);
+    }
+
+    @And("^the overall profit should be \\$(.*)$")
+    public void theOverallProfitShouldBe$(double expectedProfit) throws Throwable {
+        Integer portfolioId = theActorInTheSpotlight().recall("clientPortfolioId");
+
+        theActorInTheSpotlight().should(seeThat(ThePortfolio.overallProfitForPortfolioId(portfolioId), is(equalTo(expectedProfit))));
+
+        theActorInTheSpotlight().attemptsTo(
+                Get.resource(BDDTraderEndPoints.PortfolioProfit.path())
+                        .with(request -> request.pathParam("portfolioId", portfolioId)));
+
+        Double actualProfit = SerenityRest.lastResponse().as(Double.class);
+
+        assertThat(actualProfit).isEqualTo(expectedProfit);
     }
 }
